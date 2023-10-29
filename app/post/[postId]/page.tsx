b187f13e3eee75de240614c9170b67e3dd3809b1
Diego <diegofreeman78@gmail.com>
2023-10-29 20:35:18 +0100

    ui updated

--- app/post/[postId]/page.tsx
+++ app/post/[postId]/page.tsx
@@ -1,6 +1,9 @@
-import ActionButton from '@/app/blog/_component/ActionButton';
-import { Button, buttonVariants } from '@/components/ui/button';
+import { buttonVariants } from '@/components/ui/button';
 import { fetchSinglePost } from '@/lib/actions/post';
+import dateFormat from 'dateformat';
+import parse from 'html-react-parser';
+import Head from 'next/head';
+import Image from 'next/image';
 import Link from 'next/link';
 
 const page = async ({ params }: { params: { postId: string } }) => {
@@ -13,12 +16,15 @@ const page = async ({ params }: { params: { postId: string } }) => {
       </div>
     );
   }
+  const tags = post?.tags.join(', ');
   return (
-    <div className="w-[80%] mx-auto min-h-screen py-[100px]">
-      <div className="flex justify-between">
-        <h1 className="text-4xl font-semibold first-letter:capitalize">
-          {post?.title}
-        </h1>
+    <div className="w-[80%] mx-auto min-h-screen py-[50px] space-y-5">
+      <Head>
+        <title>{post?.title}</title>
+        <meta name="description" content={post?.meta} />
+        <meta name="keywords" content={tags} />
+      </Head>
+      <div className="flex justify-end items-center">
         <div className="max-w-fit">
           <Link
             href={`/post/edit/${post?.id}`}
@@ -27,6 +33,23 @@ const page = async ({ params }: { params: { postId: string } }) => {
             Edit
           </Link>
         </div>
+      </div>
+      {post?.thumbnail && (
+        <div className="relative aspect-video">
+          <Image src={post?.thumbnail} alt="thumbnail" fill priority />
+        </div>
+      )}
+      <div className="flex items-center justify-between">
+        {post?.tags.map((tag, i) => (
+          <span key={i} className="tag">
+            #{tag}
+          </span>
+        ))}
+        <span>{dateFormat(post?.createdAt, 'd-mmm-yyyy')}</span>
+      </div>
+      <div className="prose prose-lg max-w-full mx-auto space-y-5">
+        <h1 className="font-bold text-3xl">{post?.title}</h1>
+        {parse(post?.content)}
       </div>
     </div>
   );
--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,12 +1,16 @@
 import { fetchAllPosts } from '@/lib/actions/post';
 
 import InfiniteScrollCom from './_components/InfiniteScrollCom';
+import { Input } from '@/components/ui/input';
 type Props = {};
 
 const Post = async ({}: Props) => {
   const posts = await fetchAllPosts();
   return (
-    <div className="py-[100px] min-h-screen">
+    <div className="py-[100px] min-h-screen w-[90%] mx-auto">
+      <div>
+        <Input className="w-[200px] outline-none focus:!outline-none focus:!ring-0" />
+      </div>
       <h1 className="font-bold text-center underline mb-5">Posts</h1>
       <InfiniteScrollCom posts={posts} />
     </div>
